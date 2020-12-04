490785435c31041521726a383ae814d88cbc2312
Mohamed Emad <mohammed.emaad94@gmail.com>
2020-12-04 09:35:47 +0200

    add some extensions to make the code more readable

---
+++ GithubRepositoriesTask/Utils/Extensions.swift
@@ -0,0 +1,30 @@
+//
+//  Extensions.swift
+//  GithubRepositoriesTask
+//
+//  Created by Mohamed Emad on 12/3/20.
+//  Copyright © 2020 Mohamed Emad. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+
+    @inlinable static var baseUrl: Self {
+        "https://api.github.com/"
+    }
+    
+}
+
+extension Error {
+
+    func getErrorMessage() -> String {
+        switch self {
+        case let self as MainError:
+            return self.getErrorMessage()
+        default:
+            return self.localizedDescription
+        }
+    }
+
+}
